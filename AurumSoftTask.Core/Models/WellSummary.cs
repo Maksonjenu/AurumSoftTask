220fb2c34cf749dbc5ddfa1c17bcd490b3e300a1
Maksonjenu <genius690@mail.ru>
2026-03-09 00:21:08 +0700

    add: added summary class for well info

---
+++ AurumSoftTask.Core/Models/WellSummary.cs
@@ -0,0 +1,12 @@
+﻿namespace AurumSoftTask.Core.Models
+{
+    public record WellSummary
+
+    {
+        public string WellId { get; init; }
+        public double TotalDepth { get; init; }
+        public int IntervalCount { get; init; }
+        public double AveragePorosity { get; init; }
+        public string TopRockType { get; init; }
+    }
+}
