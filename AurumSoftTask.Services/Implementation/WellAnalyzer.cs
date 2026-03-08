8a7484b783fb37f8af99ca3845e87ee9e784ea1e
Maksonjenu <genius690@mail.ru>
2026-03-09 00:49:19 +0700

    add: implementation of analyzer and validator for wells

---
+++ AurumSoftTask.Services/Implementation/WellAnalyzer.cs
@@ -0,0 +1,43 @@
+﻿using AurumSoftTask.Core.Models;
+using AurumSoftTask.Services.Interfaces;
+
+namespace AurumSoftTask.Services.Implementations;
+
+public class WellAnalyzer : IWellAnalyzer
+{
+    public List<WellSummary> CalculateSummary(List<CsvRow> validRows)
+    {
+        var summaries = new List<WellSummary>();
+
+        var groupedByWell = validRows.GroupBy(r => r.WellId);
+
+        foreach (var group in groupedByWell)
+        {
+            var wellId = group.Key;
+            var intervalsCount = group.Count();
+            var totalDepth = group.Max(r => r.DepthTo); //pick just most deep point of well
+
+            double totalThickness = group.Sum(r => r.DepthTo - r.DepthFrom); // total thickness of all intervals in the well
+            double weightedPorosity = totalThickness == 0 // if TT is 0, no divide
+                ? 0
+                : group.Sum(r => r.Porosity * (r.DepthTo - r.DepthFrom)) / totalThickness; // sum of (porosity * thickness) for each interval
+                                                                                           // divided by total thickness to get weighted average porosity
+
+            // take most common rock type
+            // by total thickness of intervals of that rock type
+            var mostCommonRock = group
+                .GroupBy(r => r.Rock) // sort by name
+                .Select(rockGroup => new
+                {
+                    RockName = rockGroup.Key,
+                    TotalRockThickness = rockGroup.Sum(r => r.DepthTo - r.DepthFrom) // calc TT for each rock type
+                })
+                .MaxBy(x => x.TotalRockThickness)? // take most thick rock type
+                .RockName ?? "Unknown"; // no rock types in well
+
+            summaries.Add(new WellSummary(wellId, totalDepth, intervalsCount, weightedPorosity, mostCommonRock));
+        }
+
+        return summaries.OrderBy(s => s.WellId).ToList();
+    }
+}
---
+++ AurumSoftTask.Services/Implementation/WellValidator.cs
@@ -0,0 +1,88 @@
+﻿using AurumSoftTask.Core.Models;
+using AurumSoftTask.Services.Interfaces;
+
+namespace AurumSoftTask.Services.Implementations;
+
+public class WellValidator : IWellValidator
+{
+    public (List<CsvRow> ValidRows, List<ValidationError> ValidationErrors) Validate(List<CsvRow> rows)
+    {
+        var validRows = new List<CsvRow>();
+        var errors = new List<ValidationError>();
+        var structurallyValidRows = new List<CsvRow>();
+
+        // rule check for each row
+        foreach (var row in rows)
+        {
+            if (row.DepthFrom < 0)
+                errors.Add(CreateError(
+                    row, 
+                    ValidationErrorType.NegativeDepth,
+                    $"{ValidationErrorType.NegativeDepth.GetDescription()} (DepthFrom: {row.DepthFrom})."
+                    ));
+            else if (row.DepthFrom >= row.DepthTo)
+                errors.Add(CreateError(
+                    row, 
+                    ValidationErrorType.InvalidDepthRange,
+                    $"{ValidationErrorType.InvalidDepthRange.GetDescription()} (DepthFrom: {row.DepthFrom}, DepthTo: {row.DepthTo})."
+                    ));
+            else if (row.Porosity < 0 || row.Porosity > 1)
+                errors.Add(CreateError(
+                    row, 
+                    ValidationErrorType.InvalidPorosityValue,
+                    $"{ValidationErrorType.InvalidPorosityValue.GetDescription()} (Porosity: {row.Porosity})."
+                    ));
+            else if (string.IsNullOrWhiteSpace(row.Rock))
+                errors.Add(CreateError(
+                    row, 
+                    ValidationErrorType.MissingRockType,
+                    $"{ValidationErrorType.MissingRockType.GetDescription()} (Rock: '{row.Rock}')."
+                    ));
+            else
+                structurallyValidRows.Add(row); //good result
+        }
+
+        // check intervals for each well for intersection
+        var groupedByWell = structurallyValidRows.GroupBy(r => r.WellId);
+
+        foreach (var wellGroup in groupedByWell)
+        {
+            var sortedIntervals = wellGroup.OrderBy(r => r.DepthFrom).ToList();
+
+            if (sortedIntervals.Count == 0) continue;
+
+            validRows.Add(sortedIntervals[0]);
+            double currentMaxDepthTo = sortedIntervals[0].DepthTo;
+
+            for (int i = 1; i < sortedIntervals.Count; i++)
+            {
+                var current = sortedIntervals[i];
+
+                if (current.DepthFrom < currentMaxDepthTo)
+                {
+                    errors.Add(CreateError(
+                        current, 
+                        ValidationErrorType.OverlappingIntervals,
+                        $"{ValidationErrorType.OverlappingIntervals.GetDescription()} (DepthFrom: {current.DepthFrom}, overlaps with previous DepthTo: {currentMaxDepthTo})."
+                        ));
+                }
+                else
+                {
+                    validRows.Add(current);
+                    currentMaxDepthTo = Math.Max(currentMaxDepthTo, current.DepthTo);
+                }
+            }
+        }
+
+        return (validRows, errors);
+    }
+
+    private ValidationError CreateError(CsvRow row, ValidationErrorType errorType, string details) => new()
+    {
+        LineWithError = row.LineNumber,
+        WellId = row.WellId,
+        ErrorType = errorType,
+        ErrorDetails = details,
+        
+    };
+}
