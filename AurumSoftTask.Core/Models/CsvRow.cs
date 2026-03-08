56c457a68d206c0bbafbfecdea9115ab43053f7e
Maksonjenu <genius690@mail.ru>
2026-03-09 00:20:23 +0700

    add: added row dto and base row parser and validator

---
+++ AurumSoftTask.Core/Models/CsvRow.cs
@@ -0,0 +1,27 @@
+﻿namespace AurumSoftTask.Core.Models
+{
+    public record CsvRow
+    {
+        public CsvRow(int lineNumber, string wellId, double x, double y, double depthFrom, double depthTo, string rock, double porosity)
+        {
+            LineNumber = lineNumber;
+            WellId = wellId;
+            X = x;
+            Y = y;
+            DepthFrom = depthFrom;
+            DepthTo = depthTo;
+            Rock = rock;
+            Porosity = porosity;
+        }
+
+        public int LineNumber { get; init; }
+        public string WellId { get; init; }
+        public double X { get; init; }
+        public double Y { get; init; }
+        public double DepthFrom { get; init; }
+        public double DepthTo { get; init; }
+        public string Rock { get; init; }
+        public double Porosity { get; init; }
+    }
+
+}
---
+++ AurumSoftTask.Services/Implementation/CsvParser.cs
@@ -0,0 +1,69 @@
+﻿using System.Globalization;
+using AurumSoftTask.Core.Models;
+using AurumSoftTask.Services.Interfaces;
+
+namespace AurumSoftTask.Services.Implementation
+{
+    public class CsvParser : ICsvParser
+    {
+        public (List<CsvRow> Rows, List<ValidationError> ParseErrors) Parse(string filePath)
+        {
+            var rows = new List<CsvRow>();
+            var errors = new List<ValidationError>();
+
+            // first line header - skip
+            var lines = File.ReadLines(filePath).Select((text, index) => new { text, lineNumber = index + 1 }).Skip(1);
+
+            foreach (var line in lines)
+            {
+                if (string.IsNullOrWhiteSpace(line.text)) continue;
+
+                var parts = line.text.Split(';');
+                if (parts.Length != 7)
+                {
+
+                    errors.Add(CreateError(
+                        line.lineNumber,
+                        parts.FirstOrDefault(),
+                        ValidationErrorType.IncorrectColumnCount,
+                        ValidationErrorType.IncorrectColumnCount.GetDescription()
+
+                        ));
+                    continue;
+                }
+
+                // safe parse with invariant culture for decimal separator
+                bool isXParsed = double.TryParse(parts[1], CultureInfo.InvariantCulture, out double x);
+                bool isYParsed = double.TryParse(parts[2], CultureInfo.InvariantCulture, out double y);
+                bool isDepthFromParsed = double.TryParse(parts[3], CultureInfo.InvariantCulture, out double depthFrom);
+                bool isDepthToParsed = double.TryParse(parts[4], CultureInfo.InvariantCulture, out double depthTo);
+                bool isPorosityParsed = double.TryParse(parts[6], CultureInfo.InvariantCulture, out double porosity);
+
+                if (!isXParsed || !isYParsed || !isDepthFromParsed || !isDepthToParsed || !isPorosityParsed)
+                {
+                    errors.Add(CreateError(
+                        line.lineNumber,
+                        parts[0],
+                        ValidationErrorType.NumericFieldParseError,
+                        ValidationErrorType.NumericFieldParseError.GetDescription()
+
+                        ));
+                    continue;
+                }
+
+                rows.Add(new CsvRow(line.lineNumber, parts[0], x, y, depthFrom, depthTo, parts[5], porosity));
+            }
+
+            return (rows, errors);
+        }
+
+        private ValidationError CreateError(int line, string? wellId, ValidationErrorType errorType, string details) => new()
+        {
+            LineWithError = line,
+            WellId = string.IsNullOrWhiteSpace(wellId) ? "Unknown" : wellId,
+            ErrorDetails = details,
+            ErrorType = errorType
+        };
+
+    }
+}
