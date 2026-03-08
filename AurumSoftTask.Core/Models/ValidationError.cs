bbef00294e54006f92356cedb1b3fa42c2f3438a
Maksonjenu <genius690@mail.ru>
2026-03-08 21:11:13 +0700

    add: added static class with description of validation errors

--- AurumSoftTask.Core/Models/ValidationError.cs
+++ AurumSoftTask.Core/Models/ValidationError.cs
@@ -16,4 +16,16 @@
         MissingRockType,
         DuplicateWellId
     }
+
+    public static class ValidationErrorExtensions
+    {
+        public static string GetDescription(this ValidationErrorType type) => type switch
+        {
+            ValidationErrorType.MissingWellId => "Идентификатор скважины отсутствует.",
+            ValidationErrorType.InvalidDepthRange => "DepthFrom должен быть меньше DepthTo.",
+            ValidationErrorType.InvalidPorosityValue => "Пористость должна быть в диапазоне [0..1].",
+            ValidationErrorType.MissingRockType => "Название породы не может быть пустым.",
+            _ => "Неизвестная ошибка."
+        };
+    }
 }
