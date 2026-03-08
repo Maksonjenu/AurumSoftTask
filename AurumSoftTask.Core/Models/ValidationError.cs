bae19da7785793b3505428b7e148c9fe10c9bff4
Maksonjenu <genius690@mail.ru>
2026-03-08 21:08:32 +0700

    add: added error types and change type of property from string to that type

--- AurumSoftTask.Core/Models/ValidationError.cs
+++ AurumSoftTask.Core/Models/ValidationError.cs
@@ -2,8 +2,18 @@
 {
     public class ValidationError
     {
-        public int LineWithError { get; set; }
-        public string WellId { get; set; }
-        public string ErrorMessage { get; set; }
+        public int LineWithError { get; init; }
+        public string WellId { get; init; }
+        public string ErrorDetails { get; init; }
+        public ValidationErrorType ErrorType { get; init; }
+    }
+
+    public enum ValidationErrorType
+    {
+        MissingWellId,
+        InvalidDepthRange,
+        InvalidPorosityValue,
+        MissingRockType,
+        DuplicateWellId
     }
 }
