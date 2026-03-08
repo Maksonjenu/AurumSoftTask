ed78a2e773f3d85e8ee77f2466b968e9a1a63881
Maksonjenu <genius690@mail.ru>
2026-03-09 02:39:28 +0700

    add: setup app file and bind views

---
+++ AurumSoftTask.WPF/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using AurumSoftTask.Services.Implementation;
+using AurumSoftTask.WPF.ViewModels;
+using AurumSoftTask.WPF.Views;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace AurumSoftTask.WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // dependency injection without a container
+            var parser = new CsvParser();
+            var validator = new WellValidator();
+            var analyzer = new WellAnalyzer();
+
+            var mainViewModel = new MainViewModel(parser, validator, analyzer);
+
+            var mainWindow = new MainWindow
+            {
+                DataContext = mainViewModel
+            };
+
+            mainWindow.Show();
+        }
+
+    }
+
+}
