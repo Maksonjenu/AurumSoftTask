3a363d132aa8a4a47998762bc5cc5562a9ae979d
Maksonjenu <genius690@mail.ru>
2026-03-09 00:48:26 +0700

    add: added constructor for summary class

--- AurumSoftTask.Core/Models/WellSummary.cs
+++ AurumSoftTask.Core/Models/WellSummary.cs
@@ -1,12 +1,20 @@
 ﻿namespace AurumSoftTask.Core.Models
 {
     public record WellSummary
-
     {
         public string WellId { get; init; }
         public double TotalDepth { get; init; }
         public int IntervalCount { get; init; }
         public double AveragePorosity { get; init; }
         public string TopRockType { get; init; }
+
+        public WellSummary(string wellId, double totalDepth, int intervalCount, double averagePorosity, string topRockType)
+        {
+            WellId = wellId;
+            TotalDepth = totalDepth;
+            IntervalCount = intervalCount;
+            AveragePorosity = averagePorosity;
+            TopRockType = topRockType;
+        }
     }
 }
