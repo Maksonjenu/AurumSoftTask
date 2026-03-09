17b22a98a35e910314f80ca63733aa9293e988f2
Maksonjenu <genius690@mail.ru>
2026-03-09 14:07:21 +0700

    add: implementation of export service for json file

---
+++ AurumSoftTask.Services/Implementation/JsonExportService.cs
@@ -0,0 +1,19 @@
+﻿using AurumSoftTask.Services.Interfaces;
+using System.Text.Json;
+
+namespace AurumSoftTask.Services.Implementation
+{
+    public class JsonExportService : IExportService
+    {
+        public async Task ExportAsync<T>(T data, string filePath)
+        {
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true
+            };
+
+            using FileStream createStream = File.Create(filePath);
+            await JsonSerializer.SerializeAsync(createStream, data, options);
+        }
+    }
+}
