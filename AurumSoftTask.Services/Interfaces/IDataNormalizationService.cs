667027f6ec02dbc00e83672a83b455cbbd3fe8ef
Maksonjenu <genius690@mail.ru>
2026-03-12 01:42:56 +0700

    add: added base interface of data normalizer with llm

---
+++ AurumSoftTask.Services/Interfaces/IDataNormalizationService.cs
@@ -0,0 +1,8 @@
+﻿
+namespace AurumSoftTask.Services.Interfaces
+{
+    public interface IDataNormalizationService
+    {
+        Task<Dictionary<string, string>> NormalizeRockNamesAsync(IEnumerable<string> rockNames);
+    }
+}
